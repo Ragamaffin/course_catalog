2db913159e67d5f84d1df9d80a0d488a37c2b2f5
Ragamaffin <iuylskiy@gmail.com>
2021-08-06 11:33:35 +0300

    alter fk-courses-teacher_id ON DELETE to SET NULL

---
+++ migrations/m210806_082634_alter_teacher_id_fk_in_courses_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210806_082634_alter_teacher_id_fk_in_courses_table
+ */
+class m210806_082634_alter_teacher_id_fk_in_courses_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->dropForeignKey(
+            'fk-courses-teacher_id',
+            'courses'
+        );
+
+        $this->dropIndex(
+            'idx-courses-teacher_id',
+            'courses'
+        );
+
+        $this->createIndex(
+            'idx-courses-teacher_id',
+            'courses',
+            'teacher_id'
+        );
+
+        $this->addForeignKey(
+            'fk-courses-teacher_id',
+            'courses',
+            'teacher_id',
+            'teachers',
+            'id',
+            'SET NULL',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210806_082634_alter_teacher_id_fk_in_courses_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210806_082634_alter_teacher_id_fk_in_courses_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
